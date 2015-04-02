52651c560a1a4ee7160071707f65a3125f2429db
unknown <mateusz.babiuch@interia.pl>
2015-04-02 06:43:48 +0200

    Try 1

--- modules/ximgproc/samples/homology_grabcut.cpp
+++ modules/ximgproc/samples/homology_grabcut.cpp
@@ -48,9 +48,9 @@ double calculateAccuracy(const cv::Mat& output, const cv::Mat& key, int verboseL
 	double answer = (double)(tp+tn)/(tp+tn+fp+fn);
 	if (verboseLevel > 0)
 		std::cout << "Accuracy: " << answer << "\n";
-	ostringstream oss;
-	oss << "\t\tAccuracy=" << answer << "\n";
-	toLog = toLog + oss.str();
+	//ostringstream oss;
+	//oss << "\t\tAccuracy=" << answer << "\n";
+	//toLog = toLog + oss.str();
 	if (verboseLevel > 1)
 	{
 		std::cout << "True positives: " << tp << "\n";
@@ -76,9 +76,9 @@ void nextIter(const cv::Mat& image, const cv::Mat& image_mask, const cv::Mat& im
 		it_time = (((double)(finish - start)) / CLOCKS_PER_SEC);
 		if (verboseLevel > 0)
 			std::cout << "Answer for iteration " << iteration << " found in " << it_time << " seconds.\n";
-		ostringstream oss;
-		oss << "\t\tAnswer for iteration " << iteration << " found in " << it_time << " seconds.\n";
-		toLog = toLog + oss.str();
+		//ostringstream oss;
+		//oss << "\t\tAnswer for iteration " << iteration << " found in " << it_time << " seconds.\n";
+		//toLog = toLog + oss.str();
 
 		accuracy = calculateAccuracy( mask, image_mask, verboseLevel, toLog );
 	}
@@ -90,9 +90,9 @@ void nextIter(const cv::Mat& image, const cv::Mat& image_mask, const cv::Mat& im
 		it_time = (((double)(finish - start)) / CLOCKS_PER_SEC);
 		if (verboseLevel > 0)
 			std::cout << "Answer for iteration " << iteration << " found in " << it_time << " seconds.\n";
-		ostringstream oss;
-		oss << "\t\tAnswer for iteration " << iteration << " found in " << it_time << " seconds.\n";
-		toLog = toLog + oss.str();
+		//ostringstream oss;
+		//oss << "\t\tAnswer for iteration " << iteration << " found in " << it_time << " seconds.\n";
+		//toLog = toLog + oss.str();
 
 		accuracy = calculateAccuracy( mask, image_mask, verboseLevel, toLog );
 	}
@@ -118,7 +118,7 @@ int main( int argc, char** argv )
 	// Save original filename and begin log string
 	string toLog = "";
 	string original = "";
-	toLog = toLog + filename + "\n";
+	//toLog = toLog + filename + "\n";
 	original = original + filename;
 	original.substr(0, original.length()-4);
 
@@ -159,9 +159,9 @@ int main( int argc, char** argv )
 		Mat answer, bin_mask;
 		bin_mask.create( mask.size(), CV_8UC1 );
 		
-		ostringstream oss;
-		oss << "\tThreshold=" << (double)thresh/10 << "\n";
-		toLog = toLog + oss.str();
+		//ostringstream oss;
+		//oss << "\tThreshold=" << (double)thresh/10 << "\n";
+		//toLog = toLog + oss.str();
 		for (int i = 0; i < 2; i++)
 		{
 			// Perform iteration
@@ -172,14 +172,14 @@ int main( int argc, char** argv )
 			total_time += it_time;
 			bin_mask = mask & 1;
 			image.copyTo( answer, bin_mask );
-			oss = ostringstream();
-			oss << "_thresh" << (double)thresh/10 << "_it" << i << "_ac" << accuracy << "_time" << total_time << ".png";
-			string output_file = out_path + original + oss.str();
+			//oss = ostringstream();
+			//oss << "_thresh" << (double)thresh/10 << "_it" << i << "_ac" << accuracy << "_time" << total_time << ".png";
+			string output_file = out_path + original + ".png"; //+ oss.str();
 			imwrite( output_file, answer );
 		}
-		oss = ostringstream();
-		oss << "\t\tTotal time=" << total_time << "\n";
-		toLog = toLog + oss.str();
+		//oss = ostringstream();
+		//oss << "\t\tTotal time=" << total_time << "\n";
+		//toLog = toLog + oss.str();
     }
 	// Initalize log file
 	filename = argc >= 2 ? argv[1] : (char*)"log.txt";
